Corrected a few truncation quirks in the enhancement evaluation metrics computation scripts. Benchmark testset script seemingly functional

benchmark_testset.py
@@ -3,12 +3,16 @@
 #
 # ~P. Didier -- 01.12.2022
 
-import sys
+import sys, os
 import random
 import itertools
-import numpy as np
 from pathlib import Path
 from siggen.classes import *
+import pyroomacoustics as pra
+import siggen.utils as sig_ut
+import danse_toolbox.d_post as pp
+from danse_toolbox.d_base import *
+import danse_toolbox.d_core as core
 from danse_toolbox.d_classes import *
 import danse_toolbox.dataclass_methods as met
 
@@ -16,53 +20,223 @@ SIGNALSPATH = f'{Path(__file__).parent}/testing/sigs'
 
 # List of booleans parameters to consider.
 # 01.12.2022: all booleans commutations are considered.
-# - 'reverb': if True, reverberant room. Else, anechoic.
+# - 'rev': if True, reverberant room. Else, anechoic.
 # - 'SROs': if True, asynchronous WASN. Else, synchronous.
-# - 'SROestcomp': if True, estimate and compensate for SROs. Else, do not.
-# - 'simplestWASN': if True, consider a 2-nodes WASN with 1 sensor each.
+# - 'estcomp': if True, estimate and compensate for SROs. Else, do not.
+# - 'basic': if True, consider a 2-nodes WASN with 1 sensor each.
 #    Else, consider a 'realistic' WASN with more nodes, multiple sensors each.
-PARAMETERS = ['reverb', 'SROs', 'SROestcomp', 'simplestWASN']
+PARAMETERS = ['rev', 'SROs', 'estcomp', 'basic']
+
+@dataclass
+class TestParameters:
+    selfnoiseSNR: int = -50 # [dB] microphone self-noise SNR
+    referenceSensor: int = 0    # Index of the reference sensor at each node
+    wasn: WASNparameters = WASNparameters(sigDur=15)
+    danseParams: DANSEparameters = DANSEparameters()
+    exportFolder: str = f'{Path(__file__).parent}/out'  # folder to export outputs
+    seed: int = 12345
+
+    def __post_init__(self):
+        np.random.seed(self.seed)  # set random seed
+        random.seed(self.seed)  # set random seed
+        # Check consistency
+        if self.danseParams.nodeUpdating == 'sym' and\
+            any(self.wasn.SROperNode != 0):
+            raise ValueError('Simultaneous node-updating impossible in the presence of SROs.')
+
+    def save(self, exportType='pkl'):
+        """Saves dataclass to Pickle archive."""
+        met.save(self, self.exportFolder, exportType=exportType)
+
+    def load(self, foldername, dataType='pkl'):
+        """Loads dataclass to Pickle archive in folder `foldername`."""
+        return met.load(self, foldername, silent=True, dataType=dataType)
+
+BASEPARAMS = TestParameters(
+    wasn=WASNparameters(
+        sigDur=4,
+        rd=np.array([5, 5, 5]),
+        fs=16000,
+        t60=0.0,
+        nNodes=2,
+        selfnoiseSNR=99,
+        nSensorPerNode=[1, 1],
+        desiredSignalFile=[f'{SIGNALSPATH}/01_speech/{file}'\
+            for file in [
+                'speech1.wav',
+                'speech2.wav'
+            ]],
+        noiseSignalFile=[f'{SIGNALSPATH}/02_noise/{file}'\
+            for file in [
+                'whitenoise_signal_1.wav',
+                'whitenoise_signal_2.wav'
+            ]],
+        SROperNode=[0, 0]
+    ),
+    danseParams=DANSEparameters(
+        DFTsize=1024,
+        WOLAovlp=.5,
+        nodeUpdating='asy',
+        broadcastType='wholeChunk',
+        estimateSROs='CohDrift',
+        compensateSROs=False,
+        cohDrift=CohDriftParameters(
+            loop='open',
+            alpha=0.99
+        ),
+        computeCentralised=True,
+        computeLocal=True,
+    )
+)
+BASEPARAMS.danseParams.get_wasn_info(BASEPARAMS.wasn)  # complete parameters
+
 
 def main():
+    """Main function."""
 
     # Generate test battery
-    bools = list(itertools.product([0, 1], repeat=len(PARAMETERS)))
+    bools = list(itertools.product([False, True], repeat=len(PARAMETERS)))
     tests = []
     for ii in range(len(bools)):
-        tests.append(
-            dict([(PARAMETERS[jj], bools[ii][jj]) for jj in range(len(PARAMETERS))])
-        )
+        paramBools = dict([(PARAMETERS[jj], bools[ii][jj])\
+                for jj in range(len(PARAMETERS))])
+        # Avoid unnecessary tests (e.g., SRO est./comp. when no SRO present)
+        if not paramBools['SROs'] and paramBools['estcomp']:
+            pass
+        else:
+            tests.append(paramBools)
+
+    # Run tests
+    for ii in range(len(tests)):
+        print(f'######### Running test {ii+1}/{len(tests)}... #########')
+        run(tests[ii])
+    print(f'######### All testing done. #########')
 
     stop = 1
 
 
-# @dataclass
-# class TestParameters:
-#     selfnoiseSNR: int = -50 # [dB] microphone self-noise SNR
-#     referenceSensor: int = 0    # Index of the reference sensor at each node
-#     wasn: WASNparameters = WASNparameters(
-#         sigDur=15
-#     )
-#     danseParams: DANSEparameters = DANSEparameters()
-#     exportFolder: str = f'{Path(__file__).parent}/out'  # folder to export outputs
-#     seed: int = 12345
+def run(test: dict):
+    """Builds test parameters and runs the test."""
 
-#     def __post_init__(self):
-#         np.random.seed(self.seed)  # set random seed
-#         random.seed(self.seed)  # set random seed
-#         # Check consistency
-#         if self.danseParams.nodeUpdating == 'sym' and\
-#             any(self.wasn.SROperNode != 0):
-#             raise ValueError('Simultaneous node-updating impossible in the presence of SROs.')
+    # Prepare parameters
+    p = BASEPARAMS
+    if test['rev']:
+        p.wasn.t60 = 0.2
+    else:
+        p.wasn.t60 = 0.0
 
-#     def save(self, exportType='pkl'):
-#         """Saves dataclass to Pickle archive."""
-#         met.save(self, self.exportFolder, exportType=exportType)
+    if test['basic']:
+        p.wasn.nNodes = 2
+        p.wasn.nSensorPerNode = [1,1]
+    else:
+        p.wasn.nNodes = 4
+        p.wasn.nSensorPerNode = [1,3,2,5]
 
-#     def load(self, foldername, dataType='pkl'):
-#         """Loads dataclass to Pickle archive in folder `foldername`."""
-#         return met.load(self, foldername, silent=True, dataType=dataType)
+    if test['SROs']:
+        if test['basic']:
+            p.wasn.SROperNode = [0, 100]
+        else:
+            p.wasn.SROperNode = [0, 100, -100, 200]
+    else:
+        p.wasn.SROperNode = [0] * p.wasn.nNodes
 
+    if test['estcomp']:
+        p.danseParams.compensateSROs = True
+    else:
+        p.danseParams.compensateSROs = False
+    # Complete the parameters update
+    p.wasn.__post_init__()
+    p.danseParams.get_wasn_info(p.wasn)
+
+    # Build export folder name
+    foldername = '_'.join([ii for ii in list(test.keys()) if test[ii]])
+    p.exportFolder = f'{Path(__file__).parent}/out/benchmark/test{foldername}'
+
+    # Launch test
+    out = launch(p)
+    # Save `DANSEoutputs` object after metrics computation in `postprocess()`
+    out.save(foldername=p.exportFolder, light=True)
+    p.save()    # save `TestParameters` object
+
+    return out
+
+
+def launch(p: TestParameters):
+    """Launches a test, given parameters."""
+    # Build room
+    room, vad, wetSpeechAtRefSensor = sig_ut.build_room(p.wasn)
+    # Build WASN (asynchronicities, topology)
+    wasn = sig_ut.build_wasn(room, vad, wetSpeechAtRefSensor, p.wasn)
+    # DANSE
+    out, wasnUpdated = danse_it_up(wasn, p)
+    # Visualize results
+    out = postprocess(out, wasnUpdated, room, p)
+    return out
+
+
+def danse_it_up(
+    wasn: list[Node],
+    p: TestParameters
+    ) -> tuple[pp.DANSEoutputs, list[Node]]:
+    """
+    Container function for prepping signals and launching the DANSE algorithm.
+    """
+    for k in range(p.wasn.nNodes):  # for each node
+        # Derive exponential averaging factor for `Ryy` and `Rnn` updates
+        wasn[k].beta = np.exp(np.log(0.5) / \
+            (p.danseParams.t_expAvg50p * wasn[k].fs / p.danseParams.Ns))
+    # Launch DANSE
+    out, wasnUpdated = core.danse(wasn, p.danseParams)
+    return out, wasnUpdated
+
+
+def postprocess(out: pp.DANSEoutputs,
+        wasn: list[Node],
+        room: pra.room.ShoeBox,
+        p: TestParameters) -> pp.DANSEoutputs:
+    """
+    Defines the post-processing steps to be undertaken after a DANSE run.
+    Using the `danse.danse_toolbox.d_post` [abbrev. `pp`] functions.
+
+    Parameters
+    ----------
+    out : `danse.danse_toolbox.d_post.DANSEoutputs` object
+        DANSE outputs (signals, etc.)
+    wasn : list of `Node` objects
+        WASN under consideration, after DANSE processing.
+    room : `pyroomacoustics.room.ShoeBox` object
+        Acoustic scenario under consideration.
+    p : `TestParameters` object
+        Test parameters.
+    """
+
+    # Default booleans
+    runit = True   # by default, run
+    # Check whether export folder exists
+    if Path(p.exportFolder).is_dir():
+        # Check whether the folder contains something
+        if Path(p.exportFolder).stat().st_size > 0:
+            inp = input(f'The folder\n"{p.exportFolder}"\ncontains data. Overwrite? [y/[n]]:  ')
+            if inp not in ['y', 'Y']:
+                runit = False   # don't run
+                print('Aborting export.')
+    else:
+        print(f'Create export folder "{p.exportFolder}".')
+        os.makedirs(p.exportFolder)  # better than Path().mkdir()
+        # because allows subfolders in folders
+        # (https://stackoverflow.com/a/6692700)
+
+    if runit:
+        # Export .wav files
+        out.export_sounds(wasn, p.exportFolder)
+        # Plot (+ export) acoustic scenario (WASN)
+        pp.plot_asc(room, p.wasn, p.exportFolder)
+        # Plot performance metrics (+ export)
+        out.plot_perf(wasn, p.exportFolder)
+        # Plot signals at specific nodes (+ export)
+        out.plot_sigs(wasn, p.exportFolder)
+
+    return out
 
 
 if __name__ == '__main__':

danse_toolbox/d_base.py
@@ -13,6 +13,7 @@ from scipy.signal._arraytools import zero_ext
 from danse_toolbox.d_eval import DynamicMetricsParameters
 from siggen.classes import WASNparameters
 
+
 @dataclass
 class DANSEeventInstant:
     t: float = 0.   # event time instant [s]
@@ -883,7 +884,7 @@ def danse_compression_whole_chunk(yq, wHat, h, f, zqPrevious=None):
     # WOLA synthesis stage
     if zqPrevious is not None:
         # IDFT
-        zqCurr = base.back_to_time_domain(zqHat, n, axis=0)
+        zqCurr = back_to_time_domain(zqHat, n, axis=0)
         zqCurr = np.real_if_close(zqCurr)
         zqCurr *= f    # multiply by synthesis window
 
@@ -955,7 +956,7 @@ def danse_compression_few_samples(
             start=len(wIR) - L + 1,
             stop=len(wIR) + 1
         )
-        tmp = base.extract_few_samples_from_convolution(
+        tmp = extract_few_samples_from_convolution(
             idDesired,
             wIR[:, idxSensor],
             yq[:, idxSensor]
@@ -1003,7 +1004,7 @@ def dist_fct_approx(wHat, h, f, R, jitted=True):
 
     n = len(h)
 
-    wTD = base.back_to_time_domain(wHat.conj(), n, axis=0)
+    wTD = back_to_time_domain(wHat.conj(), n, axis=0)
     wTD = np.real_if_close(wTD)         
     wIR_out = np.zeros((2 * n - 1, wTD.shape[1]))
     for m in range(wTD.shape[1]):
@@ -1100,7 +1101,7 @@ def get_desired_sig_chunk(
         dhatCurr = np.einsum('ij,ij->i', w.conj(), y)
         # Transform back to time domain (WOLA processing)
         dChunCurr = normFactWOLA * win *\
-            base.back_to_time_domain(dhatCurr, len(win))
+            back_to_time_domain(dhatCurr, len(win))
         # Overlap and add construction of output time-domain signal
         if len(dChunk) < len(win):
             dChunk += np.real_if_close(dChunCurr[-len(dChunk):])
@@ -1109,13 +1110,13 @@ def get_desired_sig_chunk(
 
     elif desSigProcessingType == 'conv':
         # Compute desired signal chunk estimate using T(z) approximation
-        wIR = base.dist_fct_approx(w, win, win, Ns)
+        wIR = dist_fct_approx(w, win, win, Ns)
         # Perform convolution
         yfiltLastSamples = np.zeros((Ns, yTD.shape[-1]))
         for m in range(yTD.shape[-1]):
             # Indices required from convolution output vvv
             idDesired = np.arange(start=len(wIR) - Ns, stop=len(wIR))
-            tmp = base.extract_few_samples_from_convolution(
+            tmp = extract_few_samples_from_convolution(
                 idDesired, wIR[:, m], yTD[:, m]
             )
             yfiltLastSamples[:, m] = tmp

danse_toolbox/d_eval.py
@@ -73,7 +73,8 @@ def get_metrics(
         clean, noisy, enhan,
         fs, VAD,
         dynamic: DynamicMetricsParameters=None,
-        enhan_c=None, enhan_l=None,
+        startIdx=0,
+        startIdxCentr=None, startIdxLocal=None,
         gamma=0.2, fLen=0.03
     ):
     """
@@ -94,10 +95,12 @@ def get_metrics(
         Voice Activity Detector (1: voice + noise; 0: noise only).
     dynamic : DynamicMetricsParameters object
         Parameters for dynamic metrics estimation
-    enhan_c : [N x 1] np.ndarray (float)
-        The enhanced signal (post-CENTRALISED signal enhancement).
-    enhan_l : [N x 1] np.ndarray (float)
-        The enhanced signal (post-LOCAL signal enhancement).
+    startIdx : int
+        Sample index to start at when computing the metrics.
+    startIdxCentr : int
+        Same as above, for the centralised estimates.
+    startIdxLocal : int
+        Same as above, for the local estimates.
     gamma : float
         Gamma exponent for fwSNRseg computation.
     fLen : float
@@ -120,6 +123,20 @@ def get_metrics(
     fwSNRseg = Metric()
     myStoi = Metric()
     myPesq = Metric()
+
+    # Adapt lengths
+    clean_c = clean[startIdxCentr:]
+    clean_l = clean[startIdxLocal:]
+    clean = clean[startIdx:]
+    enhan_c = enhan[startIdxCentr:]
+    enhan_l = enhan[startIdxLocal:]
+    enhan = enhan[startIdx:]
+    # noisy_c = noisy[startIdxCentr:]
+    # noisy_l = noisy[startIdxLocal:]
+    noisy = noisy[startIdx:]
+    VAD_c = VAD[startIdxCentr:]
+    VAD_l = VAD[startIdxLocal:]
+    VAD = VAD[startIdx:]
     
     # Unweighted SNR
     snr.before = get_snr(noisy, VAD)
@@ -150,27 +167,27 @@ def get_metrics(
         myPesq.after = pesq(fs, clean, enhan, mode)
         myPesq.diff = myPesq.after - myPesq.before
         if enhan_c is not None:
-            myPesq.afterCentr = pesq(fs, clean, enhan_c, mode)
+            myPesq.afterCentr = pesq(fs, clean_c, enhan_c, mode)
         if enhan_l is not None:
-            myPesq.afterLocal = pesq(fs, clean, enhan_l, mode)
+            myPesq.afterLocal = pesq(fs, clean_l, enhan_l, mode)
     else:
         print(f'Cannot calculate PESQ for fs different from 16 or 8 kHz (current value: {fs/1e3} kHz). Keeping `myPesq` attributes at 0.')
 
     # Consider centralised and local estimates
     if enhan_c is not None:
-        snr.afterCentr = get_snr(enhan_c, VAD)
+        snr.afterCentr = get_snr(enhan_c, VAD_c)
         fwSNRseg_allFrames = get_fwsnrseg(
-            clean, enhan_c, fs, fLen, gamma
+            clean_c, enhan_c, fs, fLen, gamma
         )
         fwSNRseg.afterCentr = np.mean(fwSNRseg_allFrames)
-        myStoi.afterCentr = stoi_fcn(clean, enhan_c, fs, extended=True)
+        myStoi.afterCentr = stoi_fcn(clean_c, enhan_c, fs, extended=True)
     if enhan_l is not None:
-        snr.afterLocal = get_snr(enhan_l, VAD)
+        snr.afterLocal = get_snr(enhan_l, VAD_l)
         fwSNRseg_allFrames = get_fwsnrseg(
-            clean, enhan_l, fs, fLen, gamma
+            clean_l, enhan_l, fs, fLen, gamma
         )
         fwSNRseg.afterLocal = np.mean(fwSNRseg_allFrames)
-        myStoi.afterLocal = stoi_fcn(clean, enhan_l, fs, extended=True)
+        myStoi.afterLocal = stoi_fcn(clean_l, enhan_l, fs, extended=True)
 
     # Compute dynamic metrics
     # TODO: go through this if needed + account for centralised

danse_toolbox/d_post.py
@@ -2,19 +2,19 @@
 # visualizing DANSE outputs.
 #
 # ~created on 20.10.2022 by Paul Didier
-import sys
 import time
 import copy
 import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
-from pathlib import Path, PurePath
+from pathlib import Path
 import pyroomacoustics as pra
 from dataclasses import dataclass, fields
 from siggen.classes import Node, WASNparameters
-from danse_toolbox.d_classes import DANSEparameters, DANSEvariables
+from danse_toolbox.d_classes import DANSEvariables
 from danse_toolbox.d_eval import *
 import danse_toolbox.dataclass_methods as met
+from danse_toolbox.d_base import DANSEparameters
 
 @dataclass
 class DANSEoutputs(DANSEparameters):
@@ -150,37 +150,33 @@ def compute_metrics(
         print(f"Node {k+1}: computing speech enhancement metrics from the {startIdx[k] + 1}'th sample on (t_start = {out.tStartForMetrics[k]} s --> avoid bias due to initial filters guesses in first iterations)...")
         print(f'Computing signal enhancement evaluation metrics for node {k + 1}/{out.nNodes} (sensor {out.referenceSensor + 1}/{wasn[k].nSensors})...')
 
-        enhan_c, enhan_l = None, None
+        # Compute starting indices for centralised and localised estimates
+        startIdxCentr, startIdxLocal = None, None  # default values
         if out.computeCentralised:
             startIdxCentr[k] = int(np.floor(
                 out.tStartForMetricsCentr[k] * wasn[k].fs
             ))
             print(f"Node {k+1}: computing speech enhancement metrics for CENTRALISED PROCESSING from the {startIdxCentr[k] + 1}'th sample on (t_start = {out.tStartForMetricsCentr[k]} s).")
-            enhan_c = out.TDdesiredSignals_c[startIdxCentr[k]:, k]
         if out.computeLocal:
             startIdxLocal[k] = int(np.floor(
                 out.tStartForMetricsLocal[k] * wasn[k].fs
             ))
             print(f"Node {k+1}: computing speech enhancement metrics for LOCAL PROCESSING from the {startIdxLocal[k] + 1}'th sample on (t_start = {out.tStartForMetricsLocal[k]} s).")
-            enhan_l = out.TDdesiredSignals_l[startIdxLocal[k]:, k]
-
-    # TOFIX: TODO: the `clean` should also start at `startIdxCentr` and `startIdxLocal`
-    # --> give `startIdx`, `startIdxCentr`, and `startIdxLocal` as inputs to `get_metrics()`
 
         out0, out1, out2, out3 = get_metrics(
             # Clean speech mixture (desired signal)
-            clean=wasn[k].cleanspeechCombined[startIdx[k]:],
+            clean=wasn[k].cleanspeechCombined,
             # Microphone signals
-            noisy=wasn[k].data[startIdx[k]:, out.referenceSensor],
+            noisy=wasn[k].data[:, out.referenceSensor],
             # DANSE outputs (desired signal estimates)
-            enhan=out.TDdesiredSignals[startIdx[k]:, k],
-            # Centralised desired signal estimates
-            enhan_c=enhan_c,
-            # Local desired signal estimates
-            enhan_l=enhan_l,
-            #
+            enhan=out.TDdesiredSignals[:, k],
+            # Start indices
+            startIdx=startIdx[k],
+            startIdxCentr=startIdxCentr[k],
+            startIdxLocal=startIdxLocal[k],
+            # Other parameters
             fs=wasn[k].fs,
-            VAD=wasn[k].vadCombined[startIdx[k]:],
+            VAD=wasn[k].vadCombined,
             dynamic=out.dynMetrics,
             gamma=out.gammafwSNRseg,
             fLen=out.frameLenfwSNRseg