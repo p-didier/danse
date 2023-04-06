c50f9a0a5bb3f2f70d14b0a552f03e783119d706
p-didier <paul.didier@esat.kuleuven.be>
2023-04-06 20:08:04 +0200

    cleanuo

--- perform_tests.py
+++ perform_tests.py
@@ -4,17 +4,14 @@
 # (c) Paul Didier, SOUNDS ETN, KU Leuven ESAT STADIUS
 
 import sys
-import tests.multiple_runs
+# import tests.multiple_runs
 import tests.sandbox
 import tests.danse_robustness_to_sros
 
-# TEST_TO_RUN = tests.multiple_runs.main
-# TEST_TO_RUN = tests.sandbox.main
-TEST_TO_RUN = tests.danse_robustness_to_sros.main
-
 def main():
-    """Main function (called by default when running script)."""
-    TEST_TO_RUN()
+    tests.danse_robustness_to_sros.main()
+    # tests.sandbox.main()
+    # tests.multiple_runs.main()
 
 if __name__ == '__main__':
     sys.exit(main())
