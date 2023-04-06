bf10bb67491df04fbb78769ef7b741d40754f11a
p-didier <paul.didier@esat.kuleuven.be>
2023-04-06 20:08:35 +0200

    better figures

--- tests/danse_robustness_to_sros.py
+++ tests/danse_robustness_to_sros.py
@@ -18,9 +18,9 @@ SIGNALS_PATH = f'{Path(__file__).parent.parent}/tests/sigs'
 N_SRO_TESTS = 10
 MAX_SRO_PPM = 500
 EXPORT_FIGURES = True
-OUT_FOLDER = '20230406_tests/sros_effect'  # export path relative to `danse/out`
-SKIP_ALREADY_RUN = True  # skip tests that have already been run
-
+OUT_FOLDER = '20230406_tests/sros_effect/fc_danse_gevd'  # export path relative to `danse/out`
+SKIP_ALREADY_RUN = True  # if True, skip tests that have already been run
+SNR_PLOT_LIMITS = [-5, 20]
 
 def main():
     """Main function (called by default when running script)."""
@@ -34,15 +34,16 @@ def main():
     res = run_test_batch(cfg)
 
     # Post-process results
-    fig = post_process_results(res)
+    figs = post_process_results(res, nSensorPerNode=cfg['nSensorPerNode'])
     if EXPORT_FIGURES:
-        fig.savefig(
-            f'{Path(__file__).parent.parent}/out/{OUT_FOLDER}/sros_effect.png',
-            dpi=300
-        )
-        fig.savefig(
-            f'{Path(__file__).parent.parent}/out/{OUT_FOLDER}/sros_effect.pdf'
-        )
+        for k, fig in enumerate(figs):
+            fig.savefig(
+                f'{Path(__file__).parent.parent}/out/{OUT_FOLDER}/sros_effect_node{k + 1}.png',
+                dpi=300
+            )
+            fig.savefig(
+                f'{Path(__file__).parent.parent}/out/{OUT_FOLDER}/sros_effect_node{k + 1}.pdf'
+            )
     plt.show()
     stop = 1
 
@@ -102,7 +103,7 @@ def run_test_batch(cfg: dict):
             # Set up test parameters
             testParams = setup_test_parameters(cfg, currSROs)
             # Run test
-            res = main_sandbox(p=testParams, bypassPostprocess=True)
+            res = main_sandbox(p=testParams)
             # Extract single test results
             vals = extract_single_test_results(res)
             # Save results
@@ -181,7 +182,7 @@ def setup_test_parameters(cfg: dict, currSROs: np.ndarray) -> TestParameters:
             layoutType='vert_spinning_top',
             VADenergyDecrease_dB=35,  # [dB]
             topologyParams=TopologyParameters(
-                topologyType='user-defined',
+                topologyType=cfg['topologyType'],
                 commDistance=4.,  # [m]
                 seed=cfg['seed'],
                 userDefinedTopo=np.array([
@@ -242,7 +243,7 @@ def setup_test_parameters(cfg: dict, currSROs: np.ndarray) -> TestParameters:
     return testParams
 
 
-def post_process_results(res: list[dict]):
+def post_process_results(res: list[dict], nSensorPerNode: np.ndarray):
     """
     Post-processes the results of a test batch.
     
@@ -250,9 +251,13 @@ def post_process_results(res: list[dict]):
     ----------
     res : list[dict]
         Results.
+    nSensorPerNode : np.ndarray[int]
+        Number of sensors per node.
     
     Returns
     ----------
+    figs : list[matplotlib.figure.Figure]
+        List of figures, one for each node in the WASN.
     
     (c) Paul Didier, SOUNDS ETN, KU Leuven ESAT STADIUS
     """
@@ -283,40 +288,48 @@ def post_process_results(res: list[dict]):
             centralResSTOI[ii, k] = res[ii]['estoi'][f'Node{k+1}']['centr']
     
     # Plot
+    figs = []
     for k in range(nNodes):
         fig, axes = plt.subplots(1, 2)
-        fig.set_size_inches(6.5, 2)
+        fig.set_size_inches(10, 5)
         axes[0].plot(danseResSNR[:, k], color='C1', marker='o', label='DANSE')
         axes[0].plot(centralResSNR[:, k], color='C2', marker='s', label='Centralized')
         axes[0].hlines(localResSNR[k], 0, len(res) - 1, color='C3', linestyles='dashed', label='Local')
         axes[0].hlines(rawResSNR[k], 0, len(res) - 1, color='C0', linestyles='dashdot', label='Raw')
         axes[0].set_xlabel('SROs [PPM]')
-        axes[0].set_ylabel('SNR [dB]')
+        axes[0].set_title('SNR')
+        axes[0].set_ylabel('[dB]')
         axes[0].set_xticks(np.arange(len(res)))
         axes[0].set_xticklabels(
             [str(res[ii]['sros'][1:]) for ii in range(len(res))],
-            rotation=30
+            rotation=90
         )
-        axes[0].legend()
+        axes[0].legend(loc='lower left')
         axes[0].grid()
+        axes[0].set_ylim(SNR_PLOT_LIMITS)  # eSTOI limits
         # plt.show()
         axes[1].plot(danseResSTOI[:, k], color='C1', marker='o', label='DANSE')
         axes[1].plot(centralResSTOI[:, k], color='C2', marker='s', label='Centralized')
         axes[1].hlines(localResSTOI[k], 0, len(res) - 1, color='C3', linestyles='dashed', label='Local')
         axes[1].hlines(rawResSTOI[k], 0, len(res) - 1, color='C0', linestyles='dashdot', label='Raw')
         axes[1].set_xlabel('SROs [PPM]')
-        axes[1].set_ylabel('eSTOI')
+        axes[1].set_title('eSTOI')
         axes[1].set_xticks(np.arange(len(res)))
         axes[1].set_xticklabels(
             [str(res[ii]['sros'][1:]) for ii in range(len(res))],
-            rotation=30
+            rotation=90
         )
-        axes[1].legend()
+        axes[1].legend(loc='lower left')
         axes[1].grid()
         axes[1].set_ylim([0, 1])  # eSTOI limits
+        fig.suptitle(f'Node {k + 1} ({nSensorPerNode[k]} sensors)')
         fig.tight_layout()
 
-    return fig
+        # Save figure
+        figs.append(fig)
+        plt.close(fig)
+
+    return figs
 
 
 if __name__ == '__main__':
