722bdc614f02c3a305c944ad5ed3d2dd26cc41c9
p-didier <paul.didier@esat.kuleuven.be>
2023-05-04 15:36:21 +0200

    timing

--- perform_tests.py
+++ perform_tests.py
@@ -4,6 +4,8 @@
 # (c) Paul Didier, SOUNDS ETN, KU Leuven ESAT STADIUS
 
 import sys
+import time
+import datetime
 from pathlib import Path
 import tests.sandbox
 import tests.danse_robustness_to_sros
@@ -15,6 +17,9 @@ import tests.useless_microphones_postproc
 CONFIG_FILES_FOLDER = f'{Path(__file__).parent}/config_files'
 
 def main():
+
+    t0 = time.time()
+
     # tests.sandbox.main(cfgFilename=f'{CONFIG_FILES_FOLDER}/sandbox_config_batch.yaml')
     tests.sandbox.main(cfgFilename=f'{CONFIG_FILES_FOLDER}/sandbox_config.yaml')
     # tests.danse_robustness_to_sros.main()
@@ -23,6 +28,9 @@ def main():
     # tests.write_yaml_template.main()
     # tests.useless_microphones.main()
     # tests.useless_microphones_postproc.main()
+
+
+    print(f'\n\nTotal runtime: {str(datetime.timedelta(seconds=time.time() - t0))}.')
 
 if __name__ == '__main__':
     sys.exit(main())
