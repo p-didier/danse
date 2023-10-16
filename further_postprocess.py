ee4f945b3f7fa8080d9e310bb796d74c45ab0f65
p-didier <paul.didier@esat.kuleuven.be>
2023-10-16 15:24:15 +0200

    processing of multiple folders at once

--- further_postprocess.py
+++ further_postprocess.py
@@ -3,26 +3,37 @@
 #
 # (c) Paul Didier, SOUNDS ETN, KU Leuven ESAT STADIUS
 
+import os
 import sys
 from pathlib import Path
+import matplotlib.pyplot as plt
 import tests.postprocess_script as pp
 
 BASE_RESULTS_FOLDER = f'{Path(__file__).parent}/out/'
-SUBFOLDER = '20231006_tests/metrics_comp/test3_as2_selfnoise15dB'
+# SUBFOLDER = '20231006_tests/metrics_comp/test3_as2_selfnoise15dB'
+SUBFOLDERS = ['battery20231013_perf_asfctofSelfNoise/' + x\
+            for x in os.listdir(f'{BASE_RESULTS_FOLDER}/battery20231013_perf_asfctofSelfNoise')\
+            if x[0] != '_']
+SUBFOLDERS = SUBFOLDERS[-4:]
 
-def main():
+def main(subfolders: str=SUBFOLDERS):
     """Main function (called by default when running script)."""
     
-    # Build full path to results file
-    foldername = f'{BASE_RESULTS_FOLDER}/{SUBFOLDER}'
+    if isinstance(subfolders, str):
+        subfolders = [subfolders]
 
-    # Perform post-processing
-    pp.main(foldername, params={
-        'frameDur': None,  # [s] duration of metrics computation frame. If `None`, use entire signal, starting from the start time
-        'tmax': 20,
-        'frameShift': 1
-    })
-    pp.pp_20231006_combine_metrics(foldername, frameDur=None)
+    for sub in subfolders:
+        # Build full path to results file
+        foldername = f'{BASE_RESULTS_FOLDER}/{sub}'
+        # Perform post-processing
+        pp.main(foldername, params={
+            'frameDur': 5,   # [s] duration of metrics computation frame.
+                                # If `None`, use entire signal, starting from the start time.
+            'tmax': 30,
+            'frameShift': 1
+        })
+        # pp.pp_20231006_combine_metrics(foldername, frameDur=None)
+
 
 if __name__ == '__main__':
     sys.exit(main())
